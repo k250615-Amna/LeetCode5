5ed4206064c5cdbc7e49d290a9d35c89aa0e3fba
k250615-Amna <k250615@nu.edu.pk>
2025-12-10 21:57:53 +0500

    Update leetSub5(Q3).c

--- leetSub5(Q3).c
+++ leetSub5(Q3).c
@@ -3,34 +3,35 @@
 #include <string.h>
 
 char* concatinated(char *string1, char* string2){
-	int lenght = strlen(string1)+strlen(string2)+1;
-	char* concat = realloc(string1,lenght);
+	int lenght = strlen(string1)+strlen(string2)+1;  //stores total lenght of both strings plus one for the null terminator
+	char* concat = realloc(string1,lenght);   //makes a pointer to store memory address of the reallocated memory for each iteration of a new string
 	if(concat==NULL){
 		printf("Memory allocation failed\n");
 		return;
 	}
-	strcat(concat,string2);
+	strcat(concat,string2);   //concatinates the strings
 	return concat;
 	
 }
 int main(){
 	char stringConcat[100];
 	char *totalStrings = malloc(1);
-	if(totalStrings==NULL){
-		printf("Memory allocation failed/n");
+	if(totalStrings==NULL){     //initializes a pointer where  strings are stored
+		printf("Memory allocation failed/n");  //if the function concatinated is called five times them all of those five strings are stored here
 		return;
 	}
 	totalStrings[0]='\0';
 	while(1){
 		printf("Enter a string: ");
     	scanf("%s", stringConcat);
-    	if(strcmp(stringConcat,"Q")==0){
+    	if(strcmp(stringConcat,"Q")==0){   //terminates if user types Q
     		break;
 		}
-    	totalStrings = concatinated(totalStrings,stringConcat);
-    	printf("%s \n",totalStrings);
+    	totalStrings = concatinated(totalStrings,stringConcat);  
+    	printf("%s \n",totalStrings);   
 	}
-	free(totalStrings);
+	free(totalStrings);      //memory freed
 	printf("Memory freed");
 	
 }
+
